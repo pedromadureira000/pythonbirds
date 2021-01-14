33e6d8bf2799a0f1d9773dcd51c6251d99ff1624
phsw <pedromadureira000@gmail.com>
2021-01-14 20:56:08 -0300

    criado método cumprimento

--- oo/pesssoa.py
+++ oo/pesssoa.py
@@ -1,3 +1,12 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f"ola {id(self)}"
 
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))    #vc pode chamar o metodo apartir da classe, passando um objeto
+                                     # dessa classe como parametro self
+
+    print(p.cumprimentar())          #quando vc chama o metodo apartir do objeto vc nao precisa passar o
+                                     # objeto como parametro(self), ele sera passado implicitamente
+    print(id(p))
