fadf53b8ab35da51137e894f4754bfe78f3fc4e0
phsw <pedromadureira000@gmail.com>
2021-01-15 08:23:26 -0300

    criado e removido atributo dinamico do tipo Pessoa

--- oo/pesssoa.py
+++ oo/pesssoa.py
@@ -7,8 +7,6 @@ class Pessoa:
     def cumprimentar(self):
         return f"ola {id(self)}"
 
-
-
 if __name__ == '__main__':
     pedro = Pessoa(nome="Pedro")
     henrique = Pessoa(pedro, nome="Henrique")
@@ -16,8 +14,13 @@ if __name__ == '__main__':
     for filhos in henrique.filhos:
         print(filhos.nome)
 
-    print(Pessoa.cumprimentar(pedro))
-    print(id(pedro))
-    print(pedro.cumprimentar())
-    print(pedro.nome)
-    print(pedro.cumprimentar)
+    print("Pessoa.cumprimentar(pedro)", Pessoa.cumprimentar(pedro))
+    print("id(pedro)", id(pedro))
+    print("pedro.cumprimentar()", pedro.cumprimentar())
+    print("pedro.nome", pedro.nome)
+    print("pedro.cumprimentar", pedro.cumprimentar)
+    henrique.sobrenome = "Pedro"
+    print(henrique.__dict__)
+    print(pedro.__dict__)
+    del henrique.sobrenome      #vc pode deletar atributos dinamicamente, msm sendo um atributo definido em __init__
+    print(henrique.__dict__)    #isso não é uma boa pratica
